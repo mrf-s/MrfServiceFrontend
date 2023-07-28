38f1196895128041ea3766d37034e46f67a42a2b
NotIlya <secretman44444@gmail.com>
2023-07-29 02:31:47 +0300

    Add card route

--- src/src/app/app-routing.module.ts
+++ src/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {AppComponent} from "./app.component";
 import {ServiceCenterPageComponent} from "./pages/service-center-page/service-center-page.component";
 import {NavbarFooterPageComponent} from "./pages/navbar-footer-page/navbar-footer-page.component";
 import {LinktreePageComponent} from "./pages/linktree-page/linktree-page.component";
@@ -9,7 +8,8 @@ const routes: Routes = [
   {path: '', component: NavbarFooterPageComponent, children: [
       {path: '', component: ServiceCenterPageComponent},
     ]},
-  {path: 'card', component: LinktreePageComponent}
+  {path: 'card', component: LinktreePageComponent},
+  {path: '**', redirectTo: ''}
 ];
 
 @NgModule({
